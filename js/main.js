afed9953f661565298c5e136739326d69a8f7823
LucasLuzBr <lucasribeirodaluzbr@gmail.com>
2020-08-15 19:26:15 -0300

    Funcao calcular imposto

--- js/main.js
+++ js/main.js
@@ -23,3 +23,33 @@ function calculateGratification(){
     
     }
 }
+
+function calculateIrrf(){
+
+    if (grossSalary.value <= 1903.98){
+
+        document.querySelector('#irrf').value = "Insento"
+        valueIrrf = 0
+
+    } else if (grossSalary.value < 2826.65){
+
+        document.querySelector('#irrf').value = "7,50"
+        valueIrrf = 7.5 * grossSalary.value / 100
+
+    } else if (grossSalary.value < 3751.05){
+
+        document.querySelector('#irrf').value = "15,00"
+        valueIrrf = 15 * grossSalary.value / 100
+
+    } else if(grossSalary.value < 4664.68){
+
+        document.querySelector('#irrf').value = "22,50"
+        valueIrrf = 22.5 * grossSalary.value / 100
+
+    } else {
+
+        document.querySelector('#irrf').value = "27,50"
+        valueIrrf = 27.5 * grossSalary.value / 100
+
+    }
+}
